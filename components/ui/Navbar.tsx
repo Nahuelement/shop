07e3095ff89ba96b93dc878b5364717c46d42f22
Nahuelement <nahuel.perugi@gmail.com>
2022-11-04 13:59:27 -0300

    update

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
             <Toolbar>
                 <NextLink href='/' passHref>
                     <Link display='flex' alignItems='center'>
-                        <Typography pl={3} variant='h1'>Shopping </Typography>
+                        <Typography pl={2} variant='h1'>Shopping </Typography>
 
                     </Link>
                 </NextLink>
